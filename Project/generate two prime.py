b4cdcfc2bc13de6870d198409337aa6b2d0fd0ac
channingbabb <channingbabb@gmail.com>
2024-02-01 20:54:04 -0600

    Utilize sieve of eratostheenes

--- Project/generate two prime.py
+++ Project/generate two prime.py
@@ -1,6 +1,26 @@
 import random
 import math
 
+def sieve_of_eratosthenes(max_number):
+    """Generate all prime numbers up to max_number."""
+    is_prime = [True] * (max_number + 1)
+    is_prime[0] = is_prime[1] = False  # 0 and 1 are not prime numbers
+    primes = []
+
+    for number in range(2, int(max_number ** 0.5) + 1):
+        if is_prime[number]:
+            primes.append(number)
+            # Mark multiples of number as non-prime
+            for multiple in range(number*number, max_number + 1, number):
+                is_prime[multiple] = False
+
+    # Adding remaining prime numbers to the list
+    for number in range(int(max_number ** 0.5) + 1, max_number + 1):
+        if is_prime[number]:
+            primes.append(number)
+
+    return primes
+
 def rabin_miller(num):
     s = num - 1
     t = 0
@@ -25,7 +45,7 @@ def is_prime(num):
     if num < 2:
         return False
     
-    small_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+    small_primes = sieve_of_eratosthenes(997)
     if num in small_primes:
         return True
 
@@ -71,18 +91,6 @@ d = x[0] % phi
 print('d',d)
 
 message='topSecret'            #------------------------according to the input ifo
-message = message.upper()
-
-for M in message:  
-    M = ord(M)
-    
-    print(M)
-    C=pow(M,e,n)
-    M=pow(C,d,n)
-    print(C)
-    print(M)
-
-S=pow(M,d,n)
-print(S)
-m=pow(S,e,n)
-print(m)
+M=int(message)
+C=pow(M,e,n)
+M=pow(C,d,n)
