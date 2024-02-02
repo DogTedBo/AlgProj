7a400272a4062f125ecfccc28fd68b0c331fb166
DogTedBo <108311707+DogTedBo@users.noreply.github.com>
2024-02-01 21:25:05 -0600

    Signature

--- Project/Function.py
+++ Project/Function.py
@@ -3,5 +3,15 @@
 
 
 
-def signigture(message, key):
-    return message
+def sign_message(message, private_key):
+    checksum = sum(ord(char) for char in message) % 101
+    signature = (checksum ** private_key) % 101
+    return signature
+def verify_signature(message, signature, public_key):
+    checksum = sum(ord(char) for char in message) % 101
+    expected_checksum = (signature ** 2) % 101
+    return checksum == expected_checksum
+
+
+
+
