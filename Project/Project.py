821da8e79a46c34a281a758a5a23bdca6d0c0b05
channingbabb <channingbabb@gmail.com>
2024-02-02 13:09:47 -0600

    RSA Class

--- Project/Project.py
+++ Project/Project.py
@@ -1,12 +1,15 @@
+from RSA import RSA
 
 def main():
+    rsa_instance = RSA(1024)  # Initialize RSA instance
+
     while True:
         print("Please select your user type:")
         print("1. A public user")
         print("2. The owner of the keys")
         print("3. Exit program")
         user_type = input("Enter your choice: ")
-       
+
         if user_type == "1":
             while True:
                 print("As a public user, what would you like to do?")
@@ -14,17 +17,29 @@ def main():
                 print("2. Authenticate a digital signature")
                 print("3. Exit")
                 user_choice = input("Enter your choice: ")
-               
+
                 if user_choice == "1":
                     message = input("Enter a message: ")
-                    print("Message encrypted and sent.")
+                    encrypted_msg = rsa_instance.encrypt(message)
+                    rsa_instance.newFile(encrypted_msg)
+                    print("Message encrypted and sent")
                 elif user_choice == "2":
-                    print("There are no signatures to authenticate.")
+                    signatures = rsa_instance.getFiles("signatures")
+                    if len(signatures) == 0:
+                        print("No signatures available to authenticate.")
+                        continue
+
+                        # THIS NEEDS WORK
+                    print("The following messages are available:")
+                    for i in range(len(signatures)):
+                        print(str(signatures[i]))
+                    message_number = int(input("Enter your choice: "))
+                    print("Signature authenticated")
                 elif user_choice == "3":
                     break
                 else:
                     print("Invalid input. Please try again.")
-       
+
         elif user_type == "2":
             while True:
                 print("As the owner of the keys, what would you like to do?")
@@ -34,33 +49,36 @@ def main():
                 print("4. Generating a new set of the keys")
                 print("5. Exit")
                 user_choice = input("Enter your choice: ")
-               
+
                 if user_choice == "1":
+                    messages = rsa_instance.getFiles()
                     print("The following messages are available:")
-                    print("1. (length = 9)")
-                    Decrypted_message = input("Enter your choice: ")#No matter input which number here will print the decrypted message,maybe add else here to print invalid info
-                    print("Decrypted message: TOPSECRET")#Should output according to the input
+                    for i in range(len(messages)):
+                        print("    " + str(i+1) + ". (length = " + str(len(messages[i])) + ")")
+                    message_number = int(input("Enter your choice: "))
+                    print("Decrypted message: ", messages[message_number-1])
                 elif user_choice == "2":
-                    Signature = input("Enter a message: ")
-                    print("Message signed and sent.")
+                    message = input("Enter a message to sign: ")
+                    signature = rsa_instance.encrypt(message, rsa_instance.private_key)
+                    rsa_instance.newFile(signature, "signatures")
+                    print("Message signed and sent")
                 elif user_choice == "3":
-                    print("Public Key: <public_key>")#public key
-                    print("Private Key: <private_key>")#private key
+                    print("Public Key:", rsa_instance.public_key)
+                    print("Private Key:", rsa_instance.private_key)
                 elif user_choice == "4":
-                    print("New set of keys generated.")#New key
+                    rsa_instance = RSA(1024)  # Generate a new set of keys
+                    print("New set of keys generated.")
                 elif user_choice == "5":
                     break
                 else:
                     print("Invalid input. Please try again.")
-       
+
         elif user_type == "3":
             print("Bye for now!")
             break
-       
+
         else:
             print("Invalid input. Please try again.")
 
-
 if __name__ == "__main__":
     main()
-
