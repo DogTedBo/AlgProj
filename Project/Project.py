466f4a8cf79b57146b5b07739464fd8f83e47269
DogTedBo <108311707+DogTedBo@users.noreply.github.com>
2024-02-03 20:29:45 -0600

    improve readability

--- Project/Project.py
+++ Project/Project.py
@@ -1,10 +1,10 @@
 from rsa import RSA
 
-class RSAManager:
+class RSAManager:   # This class is used to manage the RSA class
     def __init__(self):
         self.rsa_instance = RSA(1024)
 
-    def public_user_menu(self):
+    def public_user_menu(self): # This function is used to display the menu for the public user
         while True:
             print("\nAs a public user, what would you like to do?")
             print("1. Send an encrypted message")
@@ -12,38 +12,38 @@ class RSAManager:
             print("3. Exit")
             user_choice = input("Enter your choice: ")
 
-            if user_choice == "1":
-                self.send_encrypted_message()
-            elif user_choice == "2":
+            if user_choice == "1": # This condition is used to send an encrypted message
+                self.send_encrypted_message() 
+            elif user_choice == "2": # This condition is used to authenticate a digital signature
                 self.authenticate_signature()
-            elif user_choice == "3":
+            elif user_choice == "3":    # This condition is used to exit the program
                 break
             else:
                 print("Invalid input. Please try again.")
 
-    def send_encrypted_message(self):
-        message = input("Enter a message: ")
-        encrypted_msg = self.rsa_instance.encrypt(message)
-        self.rsa_instance.newFile(encrypted_msg)
-        print("Message encrypted and sent")
+    def send_encrypted_message(self): # This function is used to send an encrypted message
+        message = input("Enter a message: ") # This line is used to take the input from the user
+        encrypted_msg = self.rsa_instance.encrypt(message) # This line is used to encrypt the message
+        self.rsa_instance.newFile(encrypted_msg) # This line is used to save the encrypted message to a file
+        print("Message encrypted and sent") # This line is used to print the message
 
-    def authenticate_signature(self):
-        signatures = self.rsa_instance.getFiles("signatures")
-        if not signatures:
-            print("No signatures available to authenticate.")
-            return
+    def authenticate_signature(self): # This function is used to authenticate a digital signature
+        signatures = self.rsa_instance.getFiles("signatures") # This line is used to get the signatures from the file
+        if not signatures: # This condition is used to check if the signatures are available
+            print("No signatures available to authenticate.") # This line is used to print the message
+            return # This line is used to return the control to the calling function
 
-        print("The following messages are available:")
-        for i, signature in enumerate(signatures, 1):
-            print(f"{i}. {signature}")
+        print("The following messages are available:") # This line is used to print the message
+        for i, signature in enumerate(signatures, 1): # This line is used to iterate through the signatures
+            print(f"{i}. {signature}") # This line is used to print the signature
 
-        message_number = int(input("Enter your choice: "))
-        print("Signature authenticated")
+        message_number = int(input("Enter your choice: ")) # This line is used to take the input from the user
+        print("Signature authenticated") # This line is used to print the message
 
-    def owner_menu(self):
+    def owner_menu(self): # This function is used to display the menu for the owner of the keys
         while True:
             print("\nAs the owner of the keys, what would you like to do?")
-            print("1. Decrypt a received message")
+            print("1. Decrypt a received message") 
             print("2. Digitally sign a message")
             print("3. Show the keys")
             print("4. Generate a new set of keys")
@@ -51,49 +51,49 @@ class RSAManager:
             user_choice = input("Enter your choice: ")
 
             if user_choice == "1":
-                self.decrypt_received_message()
+                self.decrypt_received_message() # This line is used to decrypt the received message
             elif user_choice == "2":
-                self.sign_message()
+                self.sign_message() # This line is used to sign the message
             elif user_choice == "3":
-                self.show_keys()
+                self.show_keys() # This line is used to show the keys    
             elif user_choice == "4":
-                self.generate_new_keys()
+                self.generate_new_keys() # This line is used to generate a new set of keys
             elif user_choice == "5":
                 break
             else:
                 print("Invalid input. Please try again.")
 
-    def decrypt_received_message(self):
-        messages = self.rsa_instance.getFiles()
-        if not messages:
-            print("No messages available to decrypt.")
+    def decrypt_received_message(self): # This function is used to decrypt the received message
+        messages = self.rsa_instance.getFiles() # This line is used to get the messages from the file
+        if not messages: # This condition is used to check if the messages are available
+            print("No messages available to decrypt.") 
             return
 
-        print("The following messages are available:")
-        for i, msg in enumerate(messages, 1):
-            print(f"    {i}. (length = {len(msg)})")
+        print("The following messages are available:") 
+        for i, msg in enumerate(messages, 1): # This line is used to iterate through the messages 
+            print(f"    {i}. (length = {len(msg)})") # This line is used to print the message
 
-        message_number = int(input("Enter your choice: "))
-        print("Decrypted message:", messages[message_number - 1])
+        message_number = int(input("Enter your choice: ")) # This line is used to take the input from the user
+        print("Decrypted message:", messages[message_number - 1]) # This line is used to print the message
 
-    def sign_message(self):
-        message = input("Enter a message to sign: ")
-        signature = self.rsa_instance.encrypt(message, self.rsa_instance.private_key)
-        self.rsa_instance.newFile(signature, "signatures")
+    def sign_message(self): # This function is used to sign the message
+        message = input("Enter a message to sign: ") # This line is used to take the input from the user
+        signature = self.rsa_instance.encrypt(message, self.rsa_instance.private_key) # This line is used to sign the message
+        self.rsa_instance.newFile(signature, "signatures") # This line is used to save the signature to a file
         print("Message signed and sent")
 
-    def show_keys(self):
+    def show_keys(self): # This function is used to show the keys
         print("Public Key:", self.rsa_instance.public_key)
         print("Private Key:", self.rsa_instance.private_key)
 
-    def generate_new_keys(self):
+    def generate_new_keys(self): # This function is used to generate a new set of keys
         self.rsa_instance = RSA(1024)
         print("New set of keys generated.")
 
-def main():
-    rsa_manager = RSAManager()
+def main(): # This function is used to call the main function
+    rsa_manager = RSAManager() # This line is used to create an instance of the RSAManager class
 
-    while True:
+    while True: # This line is used to run the loop
         print("\nPlease select your user type:")
         print("1. A public user")
         print("2. The owner of the keys")
@@ -101,14 +101,14 @@ def main():
         user_type = input("Enter your choice: ")
 
         if user_type == "1":
-            rsa_manager.public_user_menu()
+            rsa_manager.public_user_menu() # This line is used to display the menu for the public user
         elif user_type == "2":
-            rsa_manager.owner_menu()
+            rsa_manager.owner_menu() # This line is used to display the menu for the owner of the keys
         elif user_type == "3":
-            print("Bye for now!")
+            print("Bye for now!")   # This line is used to exit the program
             break
         else:
             print("Invalid input. Please try again.")
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": 
+    main() 
